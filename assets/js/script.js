3b92af341a145f84340c1b33a0c98d22cb892000
Elibrer <eli.brer@gmail.com>
2023-03-21 13:58:40 +1100

    Wrote JS for search button / field to input correct data for the API fetch request.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,102 @@
 var APIKey = "7ae6f66d2cf4fdbd254603d563937e4c";
 var searchBtnEl = $("#search-btn");
 var recentListEl = $("#recent-list");
-var mainTempEl = $("#main-temp");
-var mainWindEl = $("#main-wind");
-var mainHumidEl = $("#main-humid");
+var currentTempEl = $("#current-temp");
+var currentMaxTempEl = $("#current-max-temp");
+var currentMinTempEl = $("#current-min-temp");
+var currentWindEl = $("#current-wind");
+var currentHumidEl = $("#current-humid");
+var searchInputEl = $("#search-input");
 
-var cityNameHeader = $("#city-name-header");
+var cityNameHeaderEl = $("#city-name-header");
+var cityNameHeader = cityNameHeaderEl.text();
+console.log(cityNameHeader);
+
+var searchInput = "";
 
 
-function defaultPage() {
+function init() {
+    var APIUrl = "https://api.openweathermap.org/data/2.5/weather?q=Sydney" + "&appid=" + APIKey + "&units=metric";
 
+    fetch(APIUrl , {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+    })
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+    currentTempEl.text(data.main.temp + "°C")
+    currentWindEl.text(data.wind.speed + "km/h")
+    currentHumidEl.text(data.main.humidity + "%")
+
+    
+    
+    console.log(data);
+    console.log(data.main.temp);
+    });
 }
 
 
 //console.log($(searchInputEl).text());
 
 
-$(searchBtnEl).click(function (event) {
-    event.preventDefault();
-    var searchInputEl = $("#search-input");
+function citySearch() {
+    //event.preventDefault();
 
-    console.log($(searchInputEl).text());
-
-    //$(cityNameHeader).text() = $(searchInputEl).text();
-
-    // var APIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + $(searchInputEl).text() + "&appid=" + APIKey;
-
-
-    // fetch(APIUrl , {
-    //     method: 'GET', //GET is the default.
-    //     credentials: 'same-origin', // include, *same-origin, omit
-    //     redirect: 'follow', 
-    // })
-    // .then(function (response) {
-    // return response.json();
-    // })
-    // .then(function (data) {
-    // console.log(data);
-    // });
-
-    //$(cityNameHeader).text($("#search-input").val());
-
-
-  
-});
-
-
-
-function callCity(event) {
-    event.preventDefault();
-
-    var searchInput = $('#search-input').val();
-
-
+    var searchInput = searchInputEl.val();
     if (!searchInput) {
         alert('Please enter a value into the search bar.');
         return;
     }
 
-    constructSearch();
-    console.log(searchInput);
-    return(searchInput);
-}
+    var APIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + "&appid=" + APIKey + "&units=metric";
+
+    fetch(APIUrl , {
+        method: 'GET', 
+        credentials: 'same-origin', 
+        redirect: 'follow', 
+    })
+    .then(function (response) {
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error('Invalid city name');
+    })
+    .then(function (data) {
+    currentTempEl.text(data.main.temp + "°C")
+
+    searchInput = searchInput.charAt(0).toUpperCase() + searchInput.slice(1);
+    cityNameHeaderEl.text(searchInput);
+
+    console.log(data);
+    console.log(data.main.temp);
+    })
+    .catch((error) => {
+        console.error("There has been a problem with your fetch operation:", error);
+        alert("Please enter a valid city name");
+    });
+
+   
+
+
+};
+
+
+$(document).on('keypress',function(event) {
+    if(event.which == 13) {
+        citySearch();    
+    } else {
+        return;
+    }
+});
+
+console.log($("search-input").val());
+
+searchBtnEl.click(citySearch);
+
+init();
 
 
 
-
-
-// $(searchBtnEl).click(updateCity);
